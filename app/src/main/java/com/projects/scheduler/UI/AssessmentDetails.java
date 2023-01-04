605578962d77c2407b7b04b58e9aa6f9e67bd3a5
Ricky <rickydees123@gmail.com>
2023-01-03 20:17:39 -0700

    Finished (Added error checking for empty forms) (Added Landscape layouts and some color to the lists)

--- app/src/main/java/com/projects/scheduler/UI/AssessmentDetails.java
+++ app/src/main/java/com/projects/scheduler/UI/AssessmentDetails.java
@@ -1,5 +1,8 @@
 package com.projects.scheduler.UI;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,6 +15,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.projects.scheduler.R;
 import com.projects.scheduler.database.Repository;
 import com.projects.scheduler.entities.Assessment;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class AssessmentDetails extends AppCompatActivity {
 
@@ -45,6 +52,9 @@ public class AssessmentDetails extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item){
         Intent intent;
         Repository repository = new Repository(getApplication());
+        String dateString;
+        Long trigger;
+        SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy");
         switch(item.getItemId()){
             case R.id.editAssessment:
                 intent = new Intent(AssessmentDetails.this, NewAssessment.class);
@@ -57,6 +67,32 @@ public class AssessmentDetails extends AppCompatActivity {
                 intent = new Intent(AssessmentDetails.this, CourseDetails.class);
                 repository.delete(currentAssessment);
                 startActivity(intent);
+                break;
+            case R.id.notifyStart:
+                dateString = currentAssessment.getStartDate();
+                Date myStartDate = null;
+                try{
+                    myStartDate = sdf.parse(dateString);
+                } catch (ParseException e){e.printStackTrace();}
+                trigger = myStartDate.getTime();
+                intent = new Intent(AssessmentDetails.this, MyReceiver.class);
+                intent.putExtra("key", "Assessment: " + currentAssessment.getTitle() + " is today. " + currentAssessment.getStartDate());
+                PendingIntent startSender = PendingIntent.getBroadcast(AssessmentDetails.this, MainActivity.numAlert++, intent, 0);
+                AlarmManager startAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                startAlarmManager.set(AlarmManager.RTC_WAKEUP, trigger, startSender);
+                break;
+            case R.id.notifyEnd:
+                dateString = currentAssessment.getStartDate();
+                Date myEndDate = null;
+                try{
+                    myEndDate = sdf.parse(dateString);
+                } catch (ParseException e){e.printStackTrace();}
+                trigger = myEndDate.getTime();
+                intent = new Intent(AssessmentDetails.this, MyReceiver.class);
+                intent.putExtra("key", "Term: " + currentAssessment.getTitle() + " ends today. " + currentAssessment.getEndDate());
+                PendingIntent endSender = PendingIntent.getBroadcast(AssessmentDetails.this, MainActivity.numAlert++, intent, 0);
+                AlarmManager endAlarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+                endAlarmManager.set(AlarmManager.RTC_WAKEUP, trigger, endSender);
                 break;
         }
 
